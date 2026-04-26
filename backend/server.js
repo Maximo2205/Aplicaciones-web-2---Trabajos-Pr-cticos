08505f436bb25c5da7d61fcc3cd5a603a8d338d2
Nicolas Busso <nicolasbusso44@gmail.com>
2026-04-25 23:53:34 -0300

    agrego PUT para actualizar componentes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+console.log("server nuevo cargado");
 const express = require("express");
 
 const app = express();
@@ -42,6 +43,36 @@ app.post("/componentes", (req, res) => {
     componentes.push(nuevoComponente);
 
     return res.status(201).json(nuevoComponente);
+});
+
+app.delete('/componentes/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const index = componentes.findIndex(c => c.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({ error: "Componente no encontrado" });
+    }
+
+    const eliminado = componentes.splice(index, 1);
+
+    return res.json(eliminado[0]);
+});
+
+app.put("/componentes/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const { nombre, tipo } = req.body;
+
+    const index = componentes.findIndex(c => c.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({ error: "Componente no encontrado" });
+    }
+
+    componentes[index].nombre = nombre;
+    componentes[index].tipo = tipo;
+
+    return res.json(componentes[index]);
 });
 
 app.listen(PORT, () => {

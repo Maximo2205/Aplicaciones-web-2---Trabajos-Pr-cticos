900193611004d90bc3a703a203aad5e0b6bc09ef
MColomba <matiascolomba@gmail.com>
2026-06-09 21:14:03 -0300

    -

--- modules/componentes/controlador.componentes.mjs
+++ modules/componentes/controlador.componentes.mjs
@@ -2,12 +2,12 @@ import * as modelo from './modelo.componentes.mjs'
 
 export async function obtenerComponentes(req, res) {
     const productos = await modelo.obtenerComponentes()
-    res.json(productos)
+    res.status(200).json(productos)
 }
 
 export async function obtenerComponentePorId(req, res) {
     const componente = await modelo.obtenerComponentePorId(req.params.id)
-    res.json(componente)
+    res.status(200).json(componente)
 }
 
 export async function agregarComponente(req, res) {
